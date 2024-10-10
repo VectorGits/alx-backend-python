e2fd8cdc8188eeb891b351805340ee1ac27dbf29
VectorGits <davidoluremi0@gmail.com>
2024-10-10 22:30:08 +0100

    Add function to add two floating-point numbers

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
+"""
+This module provides a function to add two floating-point numbers.
+"""
+
 def add(a: float, b: float) -> float:
   """
   Add two floats and return the result
+
+  Args:
+    a (float): the first number
+    b (float): the second number
+
+  Returns:
+    float: the sum of a and b
   """
   return a + b
