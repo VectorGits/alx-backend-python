62ceeeddb833995abd4ace0a489043424e09ff6f
VectorGits <davidoluremi0@gmail.com>
2024-10-10 22:46:27 +0100

    Refactor add function to ipass pep8 style

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,15 +3,16 @@
 This module provides a function to add two floating-point numbers.
 """
 
+
 def add(a: float, b: float) -> float:
-  """
-  Add two floats and return the result
+    """
+    Add two floats and return the result
 
-  Args:
-    a (float): the first number
-    b (float): the second number
+    Args:
+      a (float): the first number
+      b (float): the second number
 
-  Returns:
-    float: the sum of a and b
-  """
-  return a + b
+    Returns:
+      float: the sum of a and b
+    """
+    return a + b
