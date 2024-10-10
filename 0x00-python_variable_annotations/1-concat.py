01b571fddc46f7a6028aceea712fb904ef191cf7
VectorGits <davidoluremi0@gmail.com>
2024-10-10 23:07:23 +0100

    Add concat function to concatenate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to add two floating-point numbers.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenate two strings and return the result
+
+    Args:
+      str1 (str): the first string
+      str2 (str): the second string
+
+    Returns:
+      str: the concatenation of str1 and str2
+    """
+    return str1 + str2
