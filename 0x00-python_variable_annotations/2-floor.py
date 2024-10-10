d1e2248e4e05fda2c8010b804bdb78cb33d0e46f
VectorGits <davidoluremi0@gmail.com>
2024-10-11 00:00:59 +0100

    Add floor function to return the floor of a float value

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+This module provides a function to return the floor of a float value.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Return the floor of a float value
+
+    Args:
+      n (float): the float value
+
+    Returns:
+      int: the floor of n
+    """
+    return int(n)
